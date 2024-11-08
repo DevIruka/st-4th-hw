fc96adaee007f1e988a53993a3aebdd952a40dc4
minjun <qkralswns1998@gmail.com>
2024-11-08 18:18:50 +0900

    구현 완.

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,8 @@
+import { useParams } from "react-router-dom";
+
+const Detail = () => {
+    const param = useParams();
+    console.log(param);
+    return <div>Detail</div>;
+};
+export default Detail;
